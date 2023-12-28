01de6b349d6209bd49c04cb17f680089b62c12c9
Chris Ham <431647+greenham@users.noreply.github.com>
2023-12-28 15:25:47 -0800

    Update About component layout and content

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,11 +1,11 @@
-import { Button, Container } from "react-bootstrap";
+import { Button, Container, Row, Col } from "react-bootstrap";
 import logo from "../assets/logo.png";
 
 function About({ id }: { id: string }) {
   return (
     <Container id={id} className="py-5 my-5">
-      <div className="row p-4 pb-0 pe-lg-0 pt-lg-5 align-items-center rounded-3 border shadow-lg">
-        <div className="col-lg-7 p-3 p-lg-5 pt-lg-3">
+      <Row className="p-4 pb-0 pe-lg-0 pt-lg-5 align-items-center rounded-3 border shadow-lg">
+        <Col lg={7} className="p-3 p-lg-5 pt-lg-3">
           <h2>👋 Hello!</h2>
           <h1 className="display-4 fw-bold lh-1 text-body-emphasis">
             I am a multi-stack web developer with over 20 years of experience
@@ -16,8 +16,9 @@ function About({ id }: { id: string }) {
             applications across the web.
             <br />
             <br />I ran my own web development and consulting business{" "}
-            <strong>for over 10 years</strong> and have worked with and for a
-            wide variety of companies.
+            <strong>for over a decade</strong> and have worked with and for a
+            wide variety of companies from mortgage lead generation to EV
+            manufacturing.
             <br />
             <br />
             My experience has taught me the value of smart, organized work,
@@ -42,11 +43,11 @@ function About({ id }: { id: string }) {
               <i className="fa-regular fa-comments px-2"></i>Contact me
             </Button>
           </div>
-        </div>
-        <div className="col-lg-4 offset-lg-1 p-0 overflow-hidden shadow-lg">
+        </Col>
+        <Col lg={4} className="offset-lg-1 p-0 overflow-hidden shadow-lg">
           <img className="rounded-lg-3" src={logo} alt="" width="500" />
-        </div>
-      </div>
+        </Col>
+      </Row>
     </Container>
   );
 }

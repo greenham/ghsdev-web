97aad2feefe7e513c0cdffbd8d1dd3f09bdaa5d7
Chris Ham <431647+greenham@users.noreply.github.com>
2023-12-29 09:27:46 -0800

    note

--- src/components/Connect.tsx
+++ src/components/Connect.tsx
@@ -1,9 +1,17 @@
-import { Button, Container, Stack } from "react-bootstrap";
+import { Alert, Button, Container, Stack } from "react-bootstrap";
 
 function Connect({ id }: { id: string }) {
   return (
     <Container id={id} className="my-5">
-      <h1 className="display-1">Connect</h1>
+      <h1 className="display-1">
+        <i className="fa-regular fa-handshake px-2"></i>Connect
+      </h1>
+      <Alert variant="info">
+        <i className="fa-solid fa-pencil px-2"></i>
+        <strong>Note</strong> - I'm always open to discussing new opportunities
+        and projects. Feel free to reach out to me on LinkedIn, GitHub, or via
+        email.
+      </Alert>
       <div className="p-5 bg-body-tertiary rounded-3">
         <Stack direction="horizontal" gap={3}>
           <Button
